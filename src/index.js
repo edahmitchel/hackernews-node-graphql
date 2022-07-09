3b36110e20e771fbcd5bcad3f0877ed058693dbb
mitchel <edahmitchel@example.com>
2022-07-09 12:42:29 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const { ApolloServer } = require('apollo-server');
+
+// 1
+const typeDefs = `
+type Query {
+    users: [User!]!
+    user(id: ID!): User
+  }
+  
+  type Mutation {
+    createUser(name: String!): User!
+  }
+  
+  type User {
+    id: ID!
+    name: String!
+  }
+`
+// const typeDefs = `
+//   type Query {
+//     info: String!
+//   }
+// `
+// 2
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`
+  }
+}
+
+// 3
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
